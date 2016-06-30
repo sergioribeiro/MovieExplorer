a4c2c409aa0ef1a28ccccafc4160931a351b1c9a
Sergio Ribeiro <sergio@sisnet.com.br>
2016-06-29 21:01:58 -0700

    First Commit

---
+++ MovieExplorer/DataViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using UIKit;
+
+namespace MovieExplorer
+{
+	public partial class DataViewController : UIViewController
+	{
+		public string DataObject {
+			get;
+			set;
+		}
+
+		public DataViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+			dataLabel.Text = DataObject;
+		}
+	}
+}
+
---
+++ MovieExplorer/RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace MovieExplorer
+{
+	[Register ("RootViewController")]
+	partial class RootViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
+
